1994827d44915ca1913246091fdc2aa4135f67b1
Abdullah Kashif <Abdullah Kashif>
2025-02-27 13:56:56 +0500

    Python variables

---
+++ Variables.py
@@ -0,0 +1,18 @@
+#variables
+
+#variable name       #variable value
+name           =       "Abdullah"
+age            =          21
+cgpa           =          3.15
+married        =          False
+
+#datatypes
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(married))
+
+#Assigning value to new variable
+cgpa2=cgpa
+print(cgpa2)#3.15
+

26fef82d16c4c35cc2bf1cb6a957e613f5beaed7
Abdullah Kashif <abdullah.kashif.softeng@gmail.com>
2025-03-03 21:16:05 +0500

    Numbers and Maths initial

---
+++ Numbers&Maths.py
@@ -0,0 +1,24 @@
+import random
+import math
+#There are 3 types of numbers in python:
+#int
+int_a=32
+#float
+float_a=64.2
+#complex
+complex_a=1+6j
+
+
+int=random.randint(1,100)
+intb=random.randrange(1,100)
+print(int,intb) #Both generate random numbers
+
+var_pi=math.pi
+print(var_pi)#3.141592653589793
+var_cos=44
+print(math.cos(var_cos))#0.9998433086476912
+var_exp=2
+print(math.exp(var_exp))#7.38905609893065
+
+
+
